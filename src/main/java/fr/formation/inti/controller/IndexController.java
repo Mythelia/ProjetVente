0043808715dcba1f3b2b20d2e36062984c42b607
Selmack <tourscher.joel@gmail.com>
2019-04-17 14:17:14 +0200

    ceci est le commit du premier controller.

---
+++ src/main/java/fr/formation/inti/controller/IndexController.java
@@ -0,0 +1,25 @@
+package fr.formation.inti.controller;
+
+import java.util.Collection;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+	protected final Log logger = LogFactory.getLog(getClass());
+
+@RequestMapping(value = "/index.htm")
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        logger.info("Returning index view");
+        return new ModelAndView("index");
+	}
+
+}

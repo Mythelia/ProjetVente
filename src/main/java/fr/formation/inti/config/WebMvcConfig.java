77afa97fbaeeac5a272d16bacf9458399a707758
Selmack <tourscher.joel@gmail.com>
2019-04-18 16:34:00 +0200

    le listener

--- src/main/java/fr/formation/inti/config/WebMvcConfig.java
+++ src/main/java/fr/formation/inti/config/WebMvcConfig.java
@@ -1,10 +1,14 @@
 package fr.formation.inti.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import fr.formation.inti.Service.SessionInterceptor;
 
 @Configuration
 @EnableWebMvc
@@ -24,5 +28,15 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
 	}
+	
+    @Bean
+    SessionInterceptor sessionInterceptor() {
+         return new SessionInterceptor();
+    }
+	
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new SessionInterceptor());
+    }
 
 }

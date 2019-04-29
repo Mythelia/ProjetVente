6f52a0bf7e9a25ebd60391025aba3497e2315f35
Mythelia <jeremy.van.migem@gmail.com>
2019-04-29 15:04:19 +0200

    a

--- src/main/java/fr/formation/inti/controller/CompteController.java
+++ src/main/java/fr/formation/inti/controller/CompteController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.sun.xml.internal.bind.v2.model.core.ID;
 
 import fr.formation.inti.entities.Login;
 import fr.formation.inti.entities.Utilisateurs;
